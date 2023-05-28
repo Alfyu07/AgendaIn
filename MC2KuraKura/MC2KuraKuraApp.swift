59df3e213d7a78f270008c2ea449fea78ade11fe
Wahyu Alfandi <w.alfa433@gmail.com>
2023-05-29 06:44:14 +0700

    Initial Commit

---
+++ MC2KuraKura/MC2KuraKuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2KuraKuraApp.swift
+//  MC2KuraKura
+//
+//  Created by Wahyu Alfandi on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2KuraKuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
